c30e21085dfb86a11d9d5dbd0054629026a231f2
hashry <hashtech03@gmail.com>
2025-05-04 15:03:41 +0100

    Skill model is added

--- access/models.py
+++ access/models.py
@@ -16,8 +16,14 @@ class Profile(AbstractUser):
     date_joined = models.DateField(_("Date Joined"), auto_now_add=True)
 
 
-
-
-
     REQUIRED_FIELDS = []
     USERNAME_FIELD = 'email'
+
+class Skill(models.Model):
+    skill_choice = (
+        ('product_manager', 'ProductManager'),('data_analytics', 'Data Analytics'),('frontend_development', 'Frontend Development'), 
+        ('backend_development', 'Backend Development'), ('uiux design', 'UI/UX Design'), ('blockchain_development', 'Blockchain Development'),
+        ('technical_writing', 'Technical Writing'), ('game_development', 'Game Development')
+    )
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name= 'profile_skill')
+    skill = models.CharField(_('Skills'),max_length=50, choices= skill_choice)
