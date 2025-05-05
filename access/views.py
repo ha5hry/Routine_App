c5f50c19cace032704fc365bb76f22f32010529a
hashry <hashtech03@gmail.com>
2025-05-05 06:26:46 +0100

    Created superuser

--- access/views.py
+++ access/views.py
@@ -1,2 +1,6 @@
-
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Profile, Skill, Follow
+from .serializers import RegisterSerializer
 # Create your views here.
