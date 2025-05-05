f098b9dce52c2f497f9f436544d35ca8da8a2298
hashry <hashtech03@gmail.com>
2025-05-05 06:28:39 +0100

    View to the access app a customized django admin page

--- access/views.py
+++ access/views.py
@@ -4,3 +4,9 @@ from rest_framework import status
 from .models import Profile, Skill, Follow
 from .serializers import RegisterSerializer
 # Create your views here.
+class RegisterApiView(APIView):
+    def post(self, request):
+        serializer = RegisterSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+        return Response(serializer.data)
