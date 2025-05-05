84eb115745df5e9b2c925cd5a900d00507d440c6
hashry <hashtech03@gmail.com>
2025-05-05 06:27:32 +0100

    Added Register Serializer

--- access/serializers.py
+++ access/serializers.py
@@ -1,1 +1,9 @@
+from rest_framework.serializers import ModelSerializer
+from .models import Profile
 
+class RegisterSerializer(ModelSerializer):
+
+    class Meta:
+        model = Profile
+        exclude = ('date_joined')
+        
