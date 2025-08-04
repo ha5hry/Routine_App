830f6bf780695800e95513e0ad59bc5ce2312010
hashry <hashtech03@gmail.com>
2025-08-04 21:43:41 +0100

    .

--- common/settings.py
+++ common/settings.py
@@ -61,6 +61,11 @@ MIDDLEWARE = [
 ]
 
 CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "https://routineapp-production.up.railway.app",
+    "http://127.0.0.1:8000",
+]
+CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = 'common.urls'
 
