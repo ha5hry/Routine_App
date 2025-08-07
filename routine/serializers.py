f0170938ef383fcb6935603cc209885a067b2c53
hashry <hashtech03@gmail.com>
2025-08-07 17:46:28 +0100

    .

--- routine/serializers.py
+++ routine/serializers.py
@@ -3,11 +3,14 @@ from . import models
 import random, string
 class RoutineSerializer(serializers.ModelSerializer):
     routine_slug = serializers.SerializerMethodField(read_only = True)
+    routine_id = serializers.SerializerMethodField(read_only = True)
     def get_routine_slug(self, obj):
          return obj.slug
+    def get_routine_id(self, obj):
+        return obj.id
     class Meta:
         model = models.Routine
-        fields = ['title', 'description', 'routine_slug']
+        fields = ['routine_id', 'title', 'description', 'routine_slug']
 
     def create(self, validated_data):
          request = self.context.get('request')
@@ -15,23 +18,24 @@ class RoutineSerializer(serializers.ModelSerializer):
          validated_data['author'] = request.user
          validated_data['routine_id'] =  random_id
          return super().create(validated_data)
-    
+
 class TodoSerializer(serializers.ModelSerializer):
     start_time = serializers.TimeField(format='%H:%M')
     end_time = serializers.TimeField(format='%H:%M')
     task_author = serializers.SerializerMethodField(read_only = True)
     task_title = serializers.SerializerMethodField (read_only = True)
-
+    task_id = serializers.SerializerMethodField(read_only=True)
 
     def get_task_author(self, obj):
         return obj.details.author.email
     def get_task_title(self, obj):
         return obj.details.title
-
+    def get_task_id(self, obj):
+        return obj.id
     class Meta:
         model = models.Todo
-        fields = ['task_title', 'task_author', 'activity_name', 'start_time', 'end_time']
-    
+        fields = ['task_id' ,'task_title', 'task_author', 'activity_name', 'start_time', 'end_time']
+
     def create(self, validated_data):
         request = self.context.get('request')
         routine_slug = self.context.get('routine_slug')
